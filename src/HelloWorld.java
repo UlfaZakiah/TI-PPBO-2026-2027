2652363a101d2e25170af70ddd7f2a5ea92d55b4
Ulfa Zakiah <ulfazakiah784@gmail.com>
2026-09-07 11:31:42 +0700

    praktikum-2: A.3. Komentar Kode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+    //Program menampilkan biodata sederhana
         System.out.println("Halo, Ulfa!");
         System.out.println("Nama: ");
         System.out.println("Ulfa");
         System.out.println();
+    //Baris di bawah ini mencetak nim mahasiswa
         System.out.println("Ini baris baru");
+        System.out.println("NIM: 2025573010065");
     }
 }
