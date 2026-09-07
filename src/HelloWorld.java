ff05abeea97438d595b11f228952596413d1c6c1
Ulfa Zakiah <ulfazakiah784@gmail.com>
2026-09-07 11:23:29 +0700

    praktikum-2: A.2. Struktur Dasar Program Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo, Ulfa!");
+        System.out.println("Nama: ");
+        System.out.println("Ulfa");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
