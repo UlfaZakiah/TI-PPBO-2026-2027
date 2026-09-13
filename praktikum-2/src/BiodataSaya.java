5e67ac9ec2d5074cbe767c0b9d64c8f7d753ca29
Ulfa Zakiah <ulfazakiah784@gmail.com>
2026-09-13 15:00:24 +0700

    Praktikum 2: Sintaks Dasar, Variabel & Operator

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,17 @@
+//Mencetak biodata mahasiswa pada baris terpisah dan pada baris yang sama
+
+public class BiodataSaya {
+    public static void main(String[] args) {
+        //Latihan 1: Mencetak Nama, NIM, dan Program Studi pada baris terpisah menggunakan println()
+        System.out.println("Nama: Ulfa Zakiah");
+        System.out.println("NIM: 2025573010065");
+        System.out.println("Program Studi: Teknik Informatika");
+
+        System.out.println(); // Baris pemisah
+
+        // Latihan 2: Cetak Nama dan NIM pada baris yang sama dengan print()
+        System.out.print("Nama: Ulfa Zakiah - ");
+        System.out.print("NIM: 2025573010065\n");
+        System.out.println("Program Studi: Teknik Informatika");
+    }
+}
---
+++ praktikum-2/src/DataPribadi.java
@@ -0,0 +1,13 @@
+public class DataPribadi {
+    public static void main(String[] args) {
+        int umur = 19;
+        double tinggiBadan = 148;
+        char inisial = 'U';
+        boolean isMahasiswaAktif = true;
+
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Status Mahasiswa Aktif: " + isMahasiswaAktif);
+    }
+}
