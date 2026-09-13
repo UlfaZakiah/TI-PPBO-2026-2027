521df06f0cb9589c54bba612175c34312fca7e8e
Ulfa Zakiah <ulfazakiah784@gmail.com>
2026-09-13 14:31:13 +0700

    praktikum-2: B.1. Deklarasi Variabel dan Tipe Data Primitif

---
+++ praktikum-2/src/VariabelDemo.java
@@ -0,0 +1,15 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        String nama = "Ulfa";
+        int umur = 19;
+        double tinggi = 148;
+        char golDarah = 'O';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
